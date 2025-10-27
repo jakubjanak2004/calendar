892f01ce1f1c947d39b3230dd70c2c30ade160dd
Jakub Janak <janakja5@fel.cvut.cz>
2025-10-27 14:12:12 +0100

    feature/user: implemented the backend infrastructure for signing up and logging into the system

---
+++ src/main/java/com/example/demo/dto/response/CalendarUserDTO.java
@@ -0,0 +1,12 @@
+package com.example.demo.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class CalendarUserDTO {
+    private String username;
+    private String firstName;
+    private String lastName;
+}
