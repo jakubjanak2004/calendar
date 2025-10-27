57c3ba17a9df7d7b31ae7b3b1cc7c403352711f0
Jakub Janak <janakja5@fel.cvut.cz>
2025-10-27 14:11:27 +0100

    feature/user: mapper implemented, also some formatting for DemoApplication and added a file tp gitignore

--- src/main/java/com/example/demo/mapper/CalendarUserMapper.java
+++ src/main/java/com/example/demo/mapper/CalendarUserMapper.java
@@ -1,16 +1,16 @@
 package com.example.demo.mapper;
 
 import com.example.demo.dto.request.SignUpDTO;
+import com.example.demo.dto.response.CalendarUserDTO;
 import com.example.demo.model.CalendarUser;
 
 public class CalendarUserMapper {
 
     public static CalendarUser toEntity(SignUpDTO signupDTO) {
-        return new CalendarUser(
-                signupDTO.getFirstName(),
-                signupDTO.getLastName(),
-                signupDTO.getUsername(),
-                signupDTO.getPassword()
-        );
+        return new CalendarUser(signupDTO.getFirstName(), signupDTO.getLastName(), signupDTO.getUsername(), signupDTO.getPassword());
+    }
+
+    public static CalendarUserDTO toDTO(CalendarUser calendarUser) {
+        return new CalendarUserDTO(calendarUser.getUsername(), calendarUser.getFirstName(), calendarUser.getLastName());
     }
 }
