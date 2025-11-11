a37cd6179da61376cbecab5c543bc76d08f31489
Jakub Janak <janakja5@fel.cvut.cz>
2025-11-11 20:29:44 +0100

    develop: SystemTest added

---
+++ src/test/java/com/example/demo/SystemTest.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+@ActiveProfiles("test")
+@SpringBootTest
+@Transactional
+public abstract class SystemTest {
+}
